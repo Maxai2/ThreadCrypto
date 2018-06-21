2ffbdc85c7890f843146eec3b9cb2527288d03bc
Хамелеон <maxali946@gmail.com>
2018-06-21 11:04:56 +0400

    Add project files.

---
+++ ThreadCrypto/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadCrypto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private string filePath;
+        public string FilePath
+        {
+            get { return filePath; }
+            set { filePath = value; OnPropertyChanged(); }
+        }
+
+
+        private ICommand fileSelect;
+        public ICommand FileSelect
+        {
+            get
+            {
+                if (fileSelect is null)
+                {
+                    fileSelect = new RelayCommand(
+                        (param) =>
+                        {
+
+                        });
+                }
+
+                return fileSelect;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
